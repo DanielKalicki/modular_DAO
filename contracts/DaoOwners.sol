233e6ad7ea1fdd931c97907481aa35f0b05f6b84
Daniel Kalicki <danielkalicki@gmail.com>
2019-09-05 21:00:11 +0200

    [modular_Dao/DaoOwners] name updated, finishFundraising function added.

--- contracts/DaoOwners.sol
+++ contracts/DaoOwners.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.1;
 
 contract DaoOwners {
     mapping (address => uint256) balances;
+    uint256 total_shares;
     
     modifier onlyOwner(){
         require(balances[msg.sender] > 0);
@@ -10,6 +11,7 @@ contract DaoOwners {
     
     constructor(uint256 _initial_shares_cnt) public{ // 100'000
         balances[msg.sender] = _initial_shares_cnt;
+        total_shares = _initial_shares_cnt;
     }
     
     function balanceOf(address _account) public view returns (uint256) {
@@ -18,6 +20,7 @@ contract DaoOwners {
     
     //Fundraising
     struct FundraisingOffer {
+        address owner;
         uint256 amount;
         uint256 percentage;
         bool accepted;
@@ -40,7 +43,7 @@ contract DaoOwners {
     }
     
     function sendFundraisingOffer(uint256 _amount, uint256 _percentage) fundraisingActive public{
-        fundraisingOffers.push(FundraisingOffer(_amount, _percentage, false));
+        fundraisingOffers.push(FundraisingOffer(msg.sender, _amount, _percentage, false));
     }
     
     function acceptFundraisingOffer(uint256 _id) fundraisingFinished onlyOwner public{
@@ -48,7 +51,22 @@ contract DaoOwners {
     }
     
     function finishFundraising() fundraisingFinished onlyOwner public {
-        
+        uint256 percentage_sum = 0;
+        for (uint i = 0; i < fundraisingOffers.length; i++){
+            if (fundraisingOffers[i].accepted){
+                percentage_sum += fundraisingOffers[i].percentage;
+            }
+        }
+        assert(percentage_sum < 100);
+        uint256 new_shares_cnt = total_shares * 100 / percentage_sum;
+        total_shares = new_shares_cnt;
+        for (uint i = 0; i < fundraisingOffers.length; i++){
+            if (fundraisingOffers[i].accepted){
+                uint256 shares_cnt = fundraisingOffers[i].percentage * total_shares / 100;
+                balances[fundraisingOffers[i].owner] = shares_cnt;
+            }
+        }
+        fundraisingOffers.length = 0;
     }
     
     //IPO
